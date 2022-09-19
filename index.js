a877413767bfcb23661436457bc53953bddf451f
Himanshu Raj <himanshuraj9194@gmail.com>
2022-09-19 13:28:44 +0530

    added forntend

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 // Import packages
 const express = require("express");
 const home = require("./routes/home");
+const path = require("path");
+const logger = require("morgan");
+const cors = require("cors");
 
 // Middlewares
 const app = express();
+app.use(logger("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
 
 // Routes
 app.use("/home", home);
+
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./frontend/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
 
 // connection
 const port = process.env.PORT || 9001;
